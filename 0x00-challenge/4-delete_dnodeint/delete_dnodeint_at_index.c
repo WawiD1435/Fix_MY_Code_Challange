6058913805016642c0a6be1a43d3adc1f3977032
kinfeurael wubishet <kinfeuraelwubishet@gmail.com>
2022-09-18 17:32:27 +0000

    fix

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -43,6 +44,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
+		(*head)->prev->prev = (*head)->prev;
 		(*head)->prev->next = (*head)->next;
 		free(*head);
 		if ((*head)->next)
